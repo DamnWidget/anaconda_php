220723fb473ae5338a4779663989fb4980a8fca1
Oscar Campos <oscar.campos@member.fsf.org>
2014-08-25 18:24:29 +0100

    added plugin version and anaconda compatibility version

---
+++ plugin_version.py
@@ -0,0 +1,9 @@
+
+# Copyright (C) 2014 - Oscar Campos <oscar.campos@member.fsf.org>
+# This program is Free Software see LICENSE file for details
+
+# Plugin version
+version = (0, 0, 1)
+
+# Minimum required anaconda version
+anaconda_version = (1, 4, 0)
