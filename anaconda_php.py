1ec12dd37e3772820a6b858970303038ea612c01
Oscar Campos <oscar.campos@member.fsf.org>
2014-08-28 00:00:46 +0100

    added version checker and release messages

--- anaconda_php.py
+++ anaconda_php.py
@@ -5,4 +5,17 @@
 """AnacondaPHP is a PHP linting plugin for Sublime Text 3
 """
 
+from .plugin_version import anaconda_required_version
+
+from anaconda.version import version as anaconda_version
+
+if anaconda_required_version > anaconda_version:
+    raise RuntimeError(
+        'AnacondaPHP requires version {} or better of anaconda but {} '
+        'is installed'.format(
+            '.'.join([str(i) for i in anaconda_required_version]),
+            '.'.join([str(i) for i in anaconda_version])
+        )
+    )
+
 from .listeners import *
